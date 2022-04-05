446864a81cebf60de5d5ad61ef3111e75c93c7c1
toth_szilvia <szylwy@gmail.com>
2022-04-05 17:23:19 +0200

    initial post on server side

--- backend/server.js
+++ backend/server.js
@@ -7,22 +7,25 @@ const path = require(`path`);
 const app = express();
 const port = "9001";
 
+const userFile = path.join(`${__dirname}/../frontend/users.json`);
+const frontendFolder = `${__dirname}/../frontend/public`;
+
+app.use(express.json());
+app.use('/pub', express.static(frontendFolder));
+
 app.get('/', (request, response, next) => { // ez a sima 
-    // console.log("Request recieved.");
+    console.log("Request recieved.");
     // response.send("Thank you for your request! This is our response.");
     response.sendFile(path.join(`${__dirname}/../frontend/index.html`)); // az index.html-t azt külön szervezzük, ez csak best practice
 });
 
-app.get('/kismacska', (request, response, next) => { 
-    response.sendFile(path.join(`${__dirname}/../frontend/somefile.json`)); 
+app.get('/admin/order-view', (request, response, next) => { // ez a sima 
+    response.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 });
 
-app.get('/something', (request, response, next) => {
-    console.log("Request recieved for something endpoint.");
-    response.send("Thank you for your request! This is our response for something endpoint.");
+app.get('/api/v1/users', (request, response) => {
+    response.sendFile(userFile);
 });
-
-const userFile = path.join(`${__dirname}/../frontend/users.json`);
 
 app.get("/api/v1/users-query", (request, response) => { // query stringek a web technológia alap része
     console.dir(request.query);
@@ -33,7 +36,6 @@ app.get("/api/v1/users-query", (request, response) => { // query stringek a web 
         response.send("Unauthorized request");
     }
 })
-
 
 app.get('/api/v1/user-params/:key', (request, response, next) => { // ez nem az lap web technológiában van benne, ez az express middleware csinálja
     console.log(request.params);
@@ -75,25 +77,19 @@ app.get(`/api/v1/users_data/:key`, (request, response, next) => {
     // }
 });
 
-const getUsersByStatus = (userStatus) => {
-    app.get(`/api/v1/users/${userStatus}`, (request, response, next) => {
+// 
+app.post('/users/new', (reqest, response) => {
         fs.readFile(userFile, (error, data) =>{
             if(error){
+                response.statusCode = 404;
                 response.send("Error just happened during opening the file.");
             } else {
                 const users = JSON.parse(data);
-                const activeUsers = users.filter( user => user.status === userStatus);
-                response.send(activeUsers);
+                console.log(reqest.body);
             }
-        });
-    });
-};
-
-// getUsersByStatus("active");
-// getUsersByStatus("passive");
-
-
-app.use('/pub', express.static(`${__dirname}/../frontend/public`)); // statikus mappa kiszolgálás, mert így nem kell egyesével sendfile-olni
+    })
+	
+});
 
 const ipAddress = `http://127.0.0.2:${port}`;
 app.listen(port, () => { 
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,3 +1,5 @@
+let root;
+
 const parseJSON = async (url) => {
     const response = await fetch(url);
     return response.json();
@@ -12,14 +14,87 @@ const userComponent = ({name, surname}) => { // object destructuring
     `;
 };
 
+const addUserComponent = () => {
+    return `
+        <div>
+            <input type= "text" name="first-name" class="first-name" placeHolder="First name">
+            <input type= "text" name="surname" class="surname" placeHolder="Surname">
+            <button id=submit-btn>Send</button>
+        </div>
+    `;
+};
+
+const submitClicked =  (event) => {
+    // event.preventDefault();
+
+    // dataToSend.append('cart', JSON.stringify(validCart));
+
+    const firstname = document.querySelector(".first-name");
+    const surname = document.querySelector(".surname");
+
+    // const dataToSend = new FormData(form);
+    // dataToSend.append('firstName', firstname.value);
+    // dataToSend.append('surname', surname.value);
+
+    // const userFormData = new FormData({
+    //     'firstName': firstname.value,
+    //     'surname': surname.value
+    // });
+
+    // console.log(userFormData.getValue('firstName'));
+    // console.log(userFormData.getValue('surname'));
+
+    const userData = {
+        'firstName': firstname.value,
+        'surname': surname.value
+    };
+    console.log(userData);
+
+    const fethSettings = {
+        method: 'POST',
+        headers: {
+            'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify(userData)
+    }
+
+    fetch('/users/new', fethSettings).then(data => {
+        if (data.status === 200){
+            messageRenderer('User has been added!');
+        }
+    }).then(async (data) => {
+        const user = await data.json();
+        root.insertAdjacentHTML(userComponent(user));
+
+    }).catch(error => {
+        console.log(error);
+    });
+
+    return response.json();
+};
+
 async function  init (event) {
+    root = document.getElementById("root");
+    if(window.location.pathname === "/admin/order-view"){ //  inspect whera re we string kezelő metódusokkal lehet megnézni, hogy az admin benne van-e a path-ban
+        console.log("We are on the admin page.");
+    } else {
+        console.log("We are on the customer/ buyer page.");
+    }
+
     const result = await parseJSON(`/api/v1/users`);
-    const root = document.getElementById("root");
 
     root.insertAdjacentHTML(
         `beforeend`, 
         result.map(user => userComponent(user)).join("")
     );
+
+    root.insertAdjacentHTML(
+        `afterend`, 
+        addUserComponent()
+    );
+
+    const submitBtn = document.getElementById("submit-btn");
+    submitBtn.addEventListener('click', submitClicked);
 
 }
 
